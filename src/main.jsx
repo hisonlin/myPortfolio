154a91a63fe389c291b971b2815aa694baa614f8
hisonlin <hisonlinca@gmail.com>
2024-05-21 17:23:39 -0700

    deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import ExperiencePage from './pages/ExperiencePage/ExperiencePage'
+import ProjectsPage from './pages/ProjectsPage/ProjectsPage'
+import ContactPage from './pages/ContactPage/ContactPage'
+
+const router= createBrowserRouter([{
+  path: 'myPortfolio/',
+  element: <HomePage />,
+  children: [
+    { path: 'myPortfolio/', element: <HomePage /> },
+    { path: 'myPortfolio/about', element: <AboutPage /> },
+    { path: 'myPortfolio/experience', element: <ExperiencePage /> },
+    { path: 'myPortfolio/projects', element: <ProjectsPage /> },
+    { path: 'myPortfolio/contact', element: <ContactPage /> },
+  ],
+
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
-      <App />
-    </BrowserRouter>
+     <RouterProvider router={router}/>
   </React.StrictMode>,
 )
