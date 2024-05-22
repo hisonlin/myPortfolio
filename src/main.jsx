b1b8d462af77144d9e15d134d718fc3de9290ba7
hisonlin <hisonlinca@gmail.com>
2024-05-21 17:34:06 -0700

    fix deployment again

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomePage from './pages/HomePage/HomePage'
-import AboutPage from './pages/AboutPage/AboutPage'
-import ExperiencePage from './pages/ExperiencePage/ExperiencePage'
-import ProjectsPage from './pages/ProjectsPage/ProjectsPage'
-import ContactPage from './pages/ContactPage/ContactPage'
-
-const router= createBrowserRouter([{
-  path: 'myPortfolio/',
-  element: <HomePage />,
-  children: [
-    { path: 'myPortfolio/', element: <HomePage /> },
-    { path: 'myPortfolio/about', element: <AboutPage /> },
-    { path: 'myPortfolio/experience', element: <ExperiencePage /> },
-    { path: 'myPortfolio/projects', element: <ProjectsPage /> },
-    { path: 'myPortfolio/contact', element: <ContactPage /> },
-  ],
-
-}])
+import { BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router}/>
+     <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
